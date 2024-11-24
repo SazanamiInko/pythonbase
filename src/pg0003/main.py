aced171c8c4c17d22a94b6b7fdd7b970354bdf4d
SazanamiInko <s.suzuki1614@gmail.com>
2024-11-24 21:05:36 +0900

    単体テスト前

---
+++ src/pg0003/main.py
@@ -0,0 +1,20 @@
+##################################
+###
+### 標準入力
+###
+###################################
+
+##変数設定開始
+
+input_value="";																					
+input_progress="あなたは誰ですか。";																					
+return_message="こんにちは{input_value}さん";																					
+
+##変数設定終了
+
+##処理部開始
+
+input_value=input(input_progress);
+print(return_message);
+
+##処理部終了
